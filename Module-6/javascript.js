496a82e6c45ddb5f23b7a2e20ebbcd0317ac6ed5
Seth Randell <sethhrandell@gmail.com>
2019-05-06 14:05:07 -0400

    Multiple update

---
+++ Module-6/javascript.js
@@ -0,0 +1,30 @@
+
+
+        document.getElementById("button1").addEventListener("click", function(){
+
+            document.getElementById("box").style.height = "500px";
+
+        });
+
+        document.getElementById("button2").addEventListener("click", function(){
+
+            document.getElementById("box").style.backgroundColor = "blue";
+            document.getElementById("box").style.height = "150px";
+
+        });
+
+        document.getElementById("button3").addEventListener("click", function(){
+
+            document.getElementById("box").style.height = "150px";
+            document.getElementById("box").style.opacity = "0.1";
+
+        });
+
+         document.getElementById("button4").addEventListener("click", function(){
+
+            document.getElementById("box").style.height = "150px";
+            document.getElementById("box").style.width = "150PX";
+            document.getElementById("box").style.backgroundColor = "orange";
+            document.getElementById("box").style.margin = "25px";
+            document.getElementById("box").style.opacity = "100";
+        });
